5d5c94fe2d488028d181c3ce8c472d937504f4bd
robasik11 <robasik11@gmail.com>
2020-01-28 13:01:16 +0100

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contraband
+{
+    class Car
+    {
+
+        public int passangers;
+        public int ContrabandAmount;
+        Random generator = new Random();
+
+        public void Carselector()
+        {
+            CleanCar CC = new CleanCar();
+            ContrabandCar CBC = new ContrabandCar();
+            if (passangers == 4)
+            {
+                CBC.Contrabandcar();
+
+            }
+            else
+            {
+
+                CC.Cleancar();
+
+            }
+
+        }
+
+        public void ParseInt()
+        {
+            int result;
+
+
+            while (true)
+            {
+                string answer = Console.ReadLine().Trim();
+
+                if (Int32.TryParse(answer, out result))
+                    break;
+
+                Console.WriteLine("Du måste skriva in ett heltal!");
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contraband
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContrabandCar CB = new ContrabandCar();
+            Car Car = new Car();
+
+            Console.WriteLine("Hur många bilar ska skapas?");
+
+            Car.ParseInt();
+
+            Console.ReadKey();
+
+            Console.WriteLine("Vilken bil vill du titta på?");
+
+
+            Car.Carselector();
+
+
+
+        }
+    }
+}
